b0d4a5fd3ea9b04916ea5d2b9490a2ece480943d
velasqvides <velasqvides@gmail.com>
2025-06-02 20:04:58 +0200

    add function to visualize radial trajs quiver2Dspokes

--- code/functions/visualizeB0maps.m
+++ code/functions/visualizeB0maps.m
@@ -1,14 +1,10 @@
 function visualizeB0maps(B0maps,minValue,maxValue)
-    % B0maps(~isfinite(B0maps)) = 2000;
-    nSlices = size(B0maps,3);
+nSlices = size(B0maps,3);
 for j =1:nSlices
-figure, imagesc(real((B0maps(:,:,j))),[minValue maxValue]);axis image; axis off; colormap('jet');
-% Set NaN values to be black
-        cmap = colormap;            % Get current colormap
-        cmap(1, :) = [0, 0, 0];     % Set the first color to black
-        colormap(cmap);             % Apply the modified colormap
-
-        % Make NaN values appear black
-        set(gca, 'Color', [0 0 0]);
+    figure, imagesc(real((B0maps(:,:,j))),[minValue maxValue]);axis image; axis off; colormap('jet');
+    cmap = colormap;            
+    cmap(1, :) = [0, 0, 0];     
+    colormap(cmap);             
+    set(gca, 'Color', [0 0 0]);
 end
 end
--- code/functions/visualizeB0maps_sliceViewer.m
+++
@@ -1,7 +0,0 @@
-function visualizeB0maps_sliceViewer(B0maps,minValue,maxValue)
-    B0maps(~isfinite(B0maps)) = 2000;
-    figure,
-    sliceViewer(real(B0maps), 'DisplayRange', [minValue,maxValue]);
-    colormap('jet');
-    colorbar;
-end
